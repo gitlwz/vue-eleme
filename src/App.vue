67e43c856a365b9eaa6a35d317b365531a09e1f2
liuwenzhu <18339967951@163.con>
2017-05-17 14:47:10 +0800

    模仿饿了么
    Signed-off-by: liuwenzhu <18339967951@163.con>

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="app">
+		<v-header></v-header>
+		<div class="tab border-1px bottom-border">
+			<div class="tab-item">
+				<router-link v-bind:to="'/goods'">
+					商品
+				</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="/ratings">
+					评论
+				</router-link>
+			</div>
+			<div class="tab-item">
+				<router-link to="/seller">
+					商家
+				</router-link>
+			</div>
+		</div>
+
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	import header from '@/components/header/header';
+
+	export default {
+		name: 'app',
+		components: {
+			"v-header": header
+		}
+	}
+</script>
+
+<style scoped>
+	.tab {
+		position: relative;
+		width: 100%;
+		height: 40px;
+		display: flex;
+		line-height: 40px;
+	}
+	
+	.tab-item {
+		flex: 1;
+		text-align: center;
+	}
+	
+	.tab .tab-item a {
+		display: block;
+		font-size: 14px;
+		color: rgb(77, 85, 93);
+	}
+	.tab .tab-item .router-link-active {
+		color: rgb(240, 20, 20);
+	}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="header">
+		<div class="content-wrapper">
+			<div class="avatar">
+				<img width="64" height="64" src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" />
+			</div>
+			<div class="content">
+				<div class="title">
+					<span class="brand"></span>
+					<span class="name">粥品香坊（回龙观）</span>
+				</div>
+				<div class="description">
+					蜂鸟专送/38分钟送达
+				</div>
+				<div class="support">
+					<span class="decrease"></span>
+					<span class="text">在线支付满28减5</span>
+				</div>
+				<div class="supports-count">
+					<span class="text">5个</span>
+					<span class="decrease">›</span>
+				</div>
+			</div>
+		</div>
+		<div class="bulletin-wrapper" @click="showDetail">
+			<span class="bulletin-title"></span>
+			<span class="bulletin-text">粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。</span>
+		</div>
+		<div class="background">
+			<img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" />
+		</div>
+		<transition name="fade">
+			<div v-if="detailShow" class="transition-detail" @click="hideDetail">
+				<div class="detail-main">
+					<h1 >粥品香坊（回龙观）</h1>
+				</div>
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+	require('./header.less')
+	export default {
+		name: 'header',
+		data() {
+			return {
+				detailShow: false
+			}
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true;
+			},
+			hideDetail() {
+				this.detailShow = false;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
